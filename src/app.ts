16bdf4e89f4009ba443c3e3a33e575a2e358c33b
Dongin Kim <miknignod@gmail.com>
2020-02-24 15:00:04 +0900

    Added express module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import { GraphQLServer } from 'graphql-yoga';
+import cors from 'cors';
 import logger from 'morgan';
 import schema from './schema';
+import { getUserId } from './utils/jwt';
 
 class App {
   public app: GraphQLServer;
@@ -14,6 +16,8 @@ class App {
 
   private middlewares = (): void => {
     this.app.express.use(logger('dev'));
+    this.app.express.use(cors());
+    this.app.express.use(getUserId);
   };
 }
 
