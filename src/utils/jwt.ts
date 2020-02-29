53500e2e83ee77df2f1a7349036e345316f203c5
Dongin Kim <miknignod@gmail.com>
2020-02-29 17:13:31 +0900

    update: express request에 user 저장

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -4,8 +4,7 @@ export const getUserId = (req, res, next) => {
   const token = req.get('X-JWT');
   if (token) {
     const decoded: any = jwt.verify(token, process.env.JWT_SECRET as string);
-    console.log(decoded);
-    req.userId = decoded.id;
+    req.user = decoded.id;
   }
   next();
 };
