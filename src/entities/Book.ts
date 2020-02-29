3b2d3aa8c1a9c5adb7b23500f3c31ccdcf6aaffc
Dongin Kim <miknignod@gmail.com>
2020-02-29 17:11:57 +0900

    update: book 스키마 변경

--- src/entities/Book.ts
+++ src/entities/Book.ts
@@ -4,17 +4,35 @@ import User from './User';
 
 @Entity()
 class Book extends Share {
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar' })
   title: string;
 
-  @Column({ type: 'varchar', length: 50 })
+  @Column({ type: 'varchar', nullable: true })
+  link: string | null | undefined;
+
+  @Column({ type: 'varchar', nullable: true })
+  image: string | null | undefined;
+
+  @Column({ type: 'varchar' })
   author: string;
 
-  @Column({ type: 'varchar', length: 50 })
-  translator: string;
+  @Column({ type: 'varchar', nullable: true })
+  translator: string | null | undefined;
 
-  @Column({ type: 'varchar', length: 255 })
-  image: string;
+  @Column({ type: 'varchar', nullable: true })
+  price: string | null | undefined;
+
+  @Column({ type: 'varchar', nullable: true })
+  publisher: string | null | undefined;
+
+  @Column({ type: 'varchar', nullable: true })
+  pubdate: string | null | undefined;
+
+  @Column({ type: 'varchar', nullable: true, unique: true })
+  isbn: string | null | undefined;
+
+  @Column({ type: 'varchar', nullable: true })
+  description: string | null | undefined;
 
   @ManyToMany((type) => User, { cascade: true, onDelete: 'CASCADE' })
   @JoinTable({
