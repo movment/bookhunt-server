4aa71a1ce972d908e132349479abee17f3e09ead
Dongin Kim <miknignod@gmail.com>
2020-02-29 22:51:18 +0900

    update: auth 추가

--- src/api/Celeb/AddCeleb/AddCeleb.resolvers.ts
+++ src/api/Celeb/AddCeleb/AddCeleb.resolvers.ts
@@ -1,33 +1,33 @@
 import { MutationAddCelebArgs, AddCelebResponse } from '../../../types/types';
 import Celeb from '../../../entities/Celeb';
+import validation from '../../../utils/validation';
 
 const resolvers = {
   Mutation: {
-    AddCeleb: async (
-      _,
-      args: MutationAddCelebArgs,
-    ): Promise<AddCelebResponse> => {
-      const { name, career, image } = args;
+    AddCeleb: validation(
+      async (_, args: MutationAddCelebArgs): Promise<AddCelebResponse> => {
+        const { name, career, image } = args;
 
-      try {
-        const celeb = new Celeb();
-        celeb.name = name;
-        celeb.career = career;
-        celeb.image = image;
+        try {
+          const celeb = new Celeb();
+          celeb.name = name;
+          celeb.career = career;
+          celeb.image = image;
 
-        await celeb.save();
-        return {
-          ok: true,
-          error: null,
-          celeb,
-        };
-      } catch (error) {
-        return {
-          ok: false,
-          error: error.message,
-        };
-      }
-    },
+          await celeb.save();
+          return {
+            ok: true,
+            error: null,
+            celeb,
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message,
+          };
+        }
+      },
+    ),
   },
 };
 
