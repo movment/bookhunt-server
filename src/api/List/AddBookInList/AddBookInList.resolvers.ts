a0a9dd84f7f59779649300c565ced66a2dd588ba
Dongin Kim <miknignod@gmail.com>
2020-03-08 16:42:43 +0900

    fix: parameter type

--- src/api/List/AddBookInList/AddBookInList.resolvers.ts
+++ src/api/List/AddBookInList/AddBookInList.resolvers.ts
@@ -4,6 +4,7 @@ import {
   MutationAddBookInListArgs,
 } from '../../../types/types';
 import Booklist from '../../../entities/Booklist';
+import Book from '../../../entities/Book';
 
 const resolvers = {
   Mutation: {
@@ -15,18 +16,32 @@ const resolvers = {
       ): Promise<AddBookInListResponse> => {
         console.log('BOOKINLIST');
         try {
-          const list = await Booklist.findOne({
-            where: {
-              id: args.listId,
-              userId: context.req.user,
-            },
-          });
+          const [list, book] = await Promise.all([
+            Booklist.findOne(args.listId, {
+              where: {
+                userId: context.req.user,
+              },
+              relations: ['books'],
+            }),
+            Book.findOne(args.bookId),
+          ]);
+
           if (!list) {
             return {
               ok: true,
               error: '내 서재가 아닙니다.',
             };
           }
+          if (!book) {
+            return {
+              ok: true,
+              error: 'No Book',
+            };
+          }
+          const index = list.books.findIndex((book) => book.id === args.bookId);
+          index !== -1 ? list.books.splice(index, 1) : list.books.push(book);
+
+          await list.save();
           return {
             ok: true,
             error: null,
