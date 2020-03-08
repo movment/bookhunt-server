1df34f145b6192af87dea55a3a13f951b8fc8a2e
Dongin Kim <miknignod@gmail.com>
2020-03-08 16:44:24 +0900

    add: 리뷰 가져오기

---
+++ src/api/Review/GetReviews/GetReviews.resolvers.ts
@@ -0,0 +1,33 @@
+import { GetReviewsResponse, QueryGetReviewsArgs } from '../../../types/types';
+import Review from '../../../entities/Review';
+
+const resolvers = {
+  Query: {
+    GetReviews: async (
+      _,
+      args: QueryGetReviewsArgs,
+      context,
+    ): Promise<GetReviewsResponse> => {
+      const { bookId } = args;
+      try {
+        const reviews: any = await Review.find({
+          where: {
+            bookId,
+          },
+        });
+        return {
+          ok: true,
+          error: null,
+          reviews,
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+        };
+      }
+    },
+  },
+};
+
+export default resolvers;
