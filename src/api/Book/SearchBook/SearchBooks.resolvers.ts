52fd2879b9c54b17c9b9970df03ca1bd66a9585f
Dongin Kim <miknignod@gmail.com>
2020-03-08 16:40:59 +0900

    update: 검색할 때마다 DB에 저장

--- src/api/Book/SearchBook/SearchBooks.resolvers.ts
+++ src/api/Book/SearchBook/SearchBooks.resolvers.ts
@@ -1,4 +1,5 @@
 import validation from '../../../utils/validation';
+import axios from 'axios';
 import {
   SearchBooksResponse,
   QuerySearchBooksArgs,
@@ -13,11 +14,55 @@ const resolvers = {
       args: QuerySearchBooksArgs,
       context,
     ): Promise<SearchBooksResponse> => {
+      const crawl = async (hangul) => {
+        try {
+          const { data } = await axios.get(
+            'https://openapi.naver.com/v1/search/book.json?query=' +
+              encodeURI(hangul) +
+              '&display=100' +
+              '&sort=count',
+            {
+              headers: {
+                'X-Naver-Client-Id': '8kdj1IIbhcAzIlKr2I34',
+                'X-Naver-Client-Secret': 'Ufs1EvtkFl',
+              },
+            },
+          );
+          for (let b of data.items) {
+            if (b.isbn) {
+              try {
+                const newBook = new Book();
+                newBook.title = b.title.replace(/<\/?[^>]+(>|$)/g, '');
+                newBook.link = b.link;
+                newBook.image = b.image?.replace('type=m1&', '');
+                newBook.author = b.author;
+                newBook.price = b.price;
+                newBook.publisher = b.publisher;
+                newBook.pubdate = b.pubdate;
+                newBook.isbn = b.isbn;
+                let description = b.description.replace(/<\/?[^>]+(>|$)/g, '');
+                if (description.length >= 255) {
+                  const index = description.lastIndexOf(' ', 252);
+                  description = description.slice(0, index).concat('...');
+                }
+                newBook.description = description;
+                await newBook.save();
+              } catch (error) {
+                console.log('DB ERROR', error.message);
+              }
+            }
+          }
+        } catch (error) {
+          console.log('Axios', error.message);
+        }
+      };
+      await crawl(args.title);
       try {
-        const books = await Book.find({
+        const books: any = await Book.find({
           where: {
             title: Like(`%${args.title}%`),
           },
+          order: { views: 'DESC' },
           take: args.max || 10,
         });
 
